3dcdc9b0bcc6937976d15ac45f19ea08e9925508
Daniel Dias <danieldias.b99@hotmail.com>
2022-08-21 15:39:42 -0300

    feat: add '/' route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import booksRouter from "./books.route";
 
 const routes = Router();
 
+routes.get('/', (_req, res) => res.status(200).json({ message: 'Rota OK!'}));
 routes.use('/books', booksRouter);
 
 export default routes;
