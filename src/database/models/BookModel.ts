8fa524a7df7a8070fff06bfd588e0b1d9ed8376d
Daniel Dias <danieldias.b99@hotmail.com>
2022-08-21 15:22:24 -0300

    feat: add sequelize book model file

---
+++ src/database/models/BookModel.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize/types";
+import { db } from "../db";
+
+export const BookModel = db.define('book', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
