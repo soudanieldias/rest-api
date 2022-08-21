993e7e2fa7da7d665d3de881e505a01bec1a6c07
Daniel Dias <danieldias.b99@hotmail.com>
2022-08-21 15:22:57 -0300

    feat: use new import from bookscontroller on booksroute

--- src/routes/books.route.ts
+++ src/routes/books.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import BooksController from '../controllers/books.controller';
+import { BooksController } from '../controllers';
 
 const booksRouter = Router();
 
