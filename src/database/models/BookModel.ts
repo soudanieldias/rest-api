557aef1832a11d6042d370e73735e0f70d2de582
Daniel Dias <danieldias.b99@hotmail.com>
2022-08-22 16:17:22 -0300

    feat: add new fields to BookModel

--- src/database/models/BookModel.ts
+++ src/database/models/BookModel.ts
@@ -19,5 +19,23 @@ export const BookModel = db.define('book', {
   author: {
     type: DataTypes.STRING,
     allowNull: false,
+  },
+  published: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  publishDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  bookCover: {
+    type: DataTypes.STRING,
+    defaultValue: `http://${process.env.HOST}:${process.env.PORT}/images/default_cover.jpg`,
   }
+},
+{
+  tableName: 'books',
+  timestamps: false,
+  underscored: true,
 });
