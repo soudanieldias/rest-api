45bbc4654026f93b7f49ba07c878290aaac43e12
Daniel Dias <danieldias.b99@hotmail.com>
2022-08-23 15:55:58 -0300

    feat: add trycatch to controller functions

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -5,30 +5,65 @@ const booksService = new BooksService();
 
 export default class BooksController {
   public async findAll(_req: Request, res: Response) {
-    const { status, message } = await booksService.findAll();
-    return res.status(status).json(message);
+    try {
+      const { status, message } = await booksService.findAll();
+
+      return res.status(status).json(message);
+
+    } catch (error) {
+
+      console.error(error);
+    }
   };
 
   public async findOne(req: Request, res: Response) {
-    const { id } = req.params;
-    const { status, message } = await booksService.findOne(id);
-    return res.status(status).json(message);
+    try {
+      const { id } = req.params;
+      const { status, message } = await booksService.findOne(id);
+
+      return res.status(status).json(message);
+
+    } catch (error) {
+
+      console.error(error);
+    }
   };
 
   public async create(req: Request, res: Response) {
-    const { status, message} = await booksService.create(req.body);
-    return res.status(status).json(message);
+    try {
+      const { status, message} = await booksService.create(req.body);
+
+      return res.status(status).json(message);
+
+    } catch (error) {
+
+      console.error(error);
+    }
   };
 
   public async update(req: Request, res: Response) {
-    const { id } = req.params;
-    const { status, message } = await booksService.update(req.body, id);
-    return res.status(status).json(message);
+    try {
+      const { id } = req.params;
+      const { status, message } = await booksService.update(req.body, id);
+
+      return res.status(status).json(message);
+
+    } catch (error) {
+
+      console.error(error);
+    }
   };
 
   public async delete(req: Request, res: Response) {
-    const { id } = req.params;
-    const {status, message } = await booksService.delete(id);
-    return res.status(status).json(message);
+    try {
+      const { id } = req.params;
+      const {status, message } = await booksService.delete(id);
+
+      return res.status(status).json(message);
+
+    } catch (error) {
+
+      console.error(error);
+    }
   };
 }
